e99e43c69ebb919f6bf9e0f0a44d4ea496005269
Alanjim Halabia <alanjim.halabia@hotmail.com>
2023-07-12 20:11:10 -0400

    feat: display future weather forecast

--- main.js
+++ main.js
@@ -6,6 +6,15 @@ var displayTemperature = document.getElementById('displayTemperature')
 var displayCondition = document.getElementById('displayCondition')
 var displayConditionImage = document.getElementById('displayConditionImage')
 
+var displayDate1 = document.getElementById('displayDate1')
+var displayTemperature1 = document.getElementById('displayTemperature1')
+var displayCondition1 = document.getElementById('displayCondition1')
+var displayConditionImage1 = document.getElementById('displayConditionImage1')
+
+var displayDate2 = document.getElementById('displayDate2')
+var displayTemperature2 = document.getElementById('displayTemperature2')
+var displayCondition2 = document.getElementById('displayCondition2')
+var displayConditionImage2 = document.getElementById('displayConditionImage2')
 
 function getWeather() {
     var cityName = document.getElementById('cityInput').value;
@@ -24,6 +33,16 @@ function getWeather() {
             displayTemperature.innerHTML = response.current.temp_c + '\xB0' + 'C';
             displayCondition.innerHTML = response.current.condition.text;
             displayConditionImage.src = 'https:' + response.current.condition.icon;
+
+            displayDate1.innerHTML = response.forecast.forecastday[1].date;
+            displayTemperature1.innerHTML = response.forecast.forecastday[1].day.avgtemp_c+ '\xB0' + 'C';
+            displayCondition1.innerHTML = response.forecast.forecastday[1].day.condition.text;
+            displayConditionImage1.src = 'https:' + response.forecast.forecastday[1].day.condition.icon;
+            
+            displayDate2.innerHTML = response.forecast.forecastday[2].date;
+            displayTemperature2.innerHTML = response.forecast.forecastday[2].day.avgtemp_c + '\xB0' + 'C';
+            displayCondition2.innerHTML = response.forecast.forecastday[2].day.condition.text;
+            displayConditionImage2.src = 'https:' + response.forecast.forecastday[2].day.condition.icon;
         })
     document.getElementById('cityInput').value = ''
 }
